b82e938c72a32780e14086fc8f065bfa9c517e06
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-09-14 17:42:28 +0200

    SONARJAVA-2459 fix test in flow computation

--- java-frontend/src/test/files/se/FlowNullTree.java
+++ java-frontend/src/test/files/se/FlowNullTree.java
@@ -3,17 +3,17 @@ abstract class A {
   void test(Object o, boolean b2) {
     try {
       try {
-        f(); // flow@f1,f4 {{...}}
+        f();
       } finally {
-        if (b2) { // flow@f1,f4 {{...}}
-          o = null; // flow@f1,f2,f3,f4 {{..}}
-          g(); // flow@f1,f3 {{Exception is thrown here.}}
+        if (b2) {
+          o = null;
+          g();
           o = new Object();
         }
       }
       h();
     } finally {
-      o.toString(); // Noncompliant [[flows=f1,f2,f3,f4]] flow@f1,f2,f3,f4
+      o.toString(); // Noncompliant
     }
   }
 
